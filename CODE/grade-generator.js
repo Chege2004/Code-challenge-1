219fa36b54ceafed0b5c66ed69ddcbb33a6d8e6f
Chege2004 <chegemanasseh2@gmail.com>
2024-07-07 23:25:42 +0300

    Added comments in between the codes

--- CODE/grade-generator.js
+++ CODE/grade-generator.js
@@ -1,9 +1,15 @@
 function gradeCalculator() {
-    const studentMarks = prompt("Enter your marks");
+    //Prompt student for marks
+    const marks = prompt("Enter your marks");
+    //Check if user entered a value as well as convert the input to a number
+    
+    const studentMarks = parseInt(marks);
     if (studentMarks === null || studentMarks === ""){
         alert("Please enter  a value");
+        //Check if the marks are between 0 and 100
     }else if (studentMarks < 0 || studentMarks > 100) {
         alert("Please enter a value between 0 and 100");
+        //Provide the appropriate grade based on the input marks
     }else if (studentMarks >79) {
         console.log("Grade A");
     }else if (studentMarks >=60 && studentMarks <=79) {
--- CODE/speed-detector.js
+++ CODE/speed-detector.js
@@ -1,5 +1,7 @@
 function speedDetector(){
+    //Prompt user for speed
     const speedString = prompt("Enter your speed");
+    //Check if user entered a value as well as convert the input to a number
     const speed = parseInt(speedString);
     const speedLimit = 70;
     const kmPerPoint = 5;
@@ -11,11 +13,13 @@ function speedDetector(){
     else if (isNaN(speed) || speed < 0){
         alert("Please enter a value greater than 0 and in number format");
         return;
+        //Check if the speed is below the speed limit
     }
     else if (speed <=speedLimit){
         console.log("Ok");
         return;
     }
+    //Check if the speed is above the speed limit
     const overSpeed = speed - speedLimit;
     const points = Math.floor((speed - speedLimit)/kmPerPoint);
     console.log(points + " points");
